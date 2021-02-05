50efa0b2187b8fa16d47a3f194d6a56ece64bf8b
leyi <leyi@myun.info>
2021-02-05 11:58:42 +0800

    fix bug

--- check-sys.sh
+++ check-sys.sh
@@ -2,7 +2,7 @@
 
 #计算cpu使用率, -n number：指定在结束前应该产生的最大迭代次数或帧数,一般迭代次数越大，获取到的cpu使用率越准确; 本脚本中是迭代了5次，"-n5" 
 cpu=`top -b -n5 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' '{split($1, vs, ","); v=vs[length(vs)]; sub(/\s+/, "", v);sub(/\s+/, "", v); printf "%d", 100-v;}'`
-echo 'cpu使用率: '$cpu'%'
+echo 'cpu使用率: '$cpu'%\n'
 
 #统计内存使用率
 mem_used_persent=`free -m | awk -F '[ :]+' 'NR==2{printf "%d", ($3)/$2*100}'`
