7189314e9c12acb207b50f6bd55a2f7ab545502e
leyi <leyi@myun.info>
2021-06-23 13:33:55 +0800

    新增pm2-logrotate模块

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -13,5 +13,14 @@ echo 'We are going to install pm2 for you... '
 npm install -g pm2 yarn --registry=https://registry.npm.taobao.org
 
 clear
+echo 'We are going to install pm2-logrotate for you... '
+pm2 install pm2-logrotate
+pm2 set pm2-logrotate:max_size 10M
+pm2 set pm2-logrotate:dateFormat YYYY-MM-DD_HH-mm-ss
+pm2 set pm2-logrotate:workerInterval 3600
+pm2 set pm2-logrotate:rotateInterval 0 0 * * *
+pm2 set pm2-logrotate:TZ Asia/Shanghai
+
+clear
 echo "NodeJS is now installed @ version:"
 node --version
