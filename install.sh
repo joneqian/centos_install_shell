1b948a0a32e3572f715b79d968225e62dfe9cfb8
leyi <leyi@myun.info>
2024-01-31 14:47:15 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+###
+ # @Author: leyi leyi@myun.info
+ # @Date: 2020-04-21 22:09:28
+ # @LastEditors: leyi leyi@myun.info
+ # @LastEditTime: 2024-01-31 14:47:06
+ # @FilePath: /centos_install_shell/install.sh
+ # @Description: 
+ # 
+ # Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+### 
 
 # install tools:vim wget git
 clear
 echo 'We are going to install vim,wget and git... '
 yum update -y
-yum install -y vim wget git
+yum install -y vim wget git epel-release
+yum install -y htop
 
 clear
 echo 'We are going to enable firewalld... '
